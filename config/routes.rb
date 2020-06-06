fff1845e5aa59aefb6d0bedb2efd67e0acf63512
Ben Daniels <benbanj@hotmail.com>
2020-06-07 00:57:24 +0100

    Adding UserSessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/login', to: 'user_sessions#new'
+  post 'users/create', to: 'user_sessions#create'
+  get 'users/logout', to: 'user_sessions#destroy'
   resources :users, only: [:new, :create, :show]
 end
