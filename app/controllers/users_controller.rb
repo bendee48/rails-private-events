1aea0f3be9e27f1fcd28b35ef3b56b5b0a98f632
Ben Daniels <benbanj@hotmail.com>
2020-06-06 18:51:16 +0100

    Edit User#create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,13 +7,14 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     
     if @user.save
-      redirect_to user_path(@user.id)
+      redirect_to @user
     else
       render :new
     end
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   private
