068ef0ae9e257d10be1c81ae79e637e41c12f7a6
Ben Daniels <benbanj@hotmail.com>
2020-06-11 17:46:39 +0100

    Split events into past and upcoming

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,22 @@
 class User < ApplicationRecord
-  has_many :created_events, foreign_key: :creator_id, class_name: "Event"
+  has_many :created_events, foreign_key: :creator_id, class_name: "Event", dependent: :destroy
 
-  has_many :event_attendances, foreign_key: :attendee_id
-  has_many :attended_events, through: :event_attendances
+  has_many :event_attendances, foreign_key: :attendee_id, dependent: :destroy
+  has_many :attended_events, through: :event_attendances, dependent: :destroy
+
+  def upcoming_events
+    split_events.first
+  end
+
+  def past_events
+    split_events.last
+  end
+
+  private 
+
+  def split_events
+    attended_events.partition do |event|
+      event.date > Time.now
+    end
+  end
 end
