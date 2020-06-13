fdce2725fbc5c4289fbbcb07113cd1fe20b7a90a
Ben Daniels <benbanj@hotmail.com>
2020-06-13 21:00:24 +0100

    Sorting/ formatiing dates

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,6 +4,7 @@ class Event < ApplicationRecord
   has_many :event_attendances, foreign_key: :attended_event_id, dependent: :destroy
   has_many :attendees, through: :event_attendances, dependent: :destroy
 
-  scope :past, -> { where('date < ?', Time.now) }
-  scope :upcoming, -> { where('date > ?', Time.now) }
+  scope :date_order, -> { order(:date) }
+  scope :past, -> { where('date < ?', Time.now).order(date: :desc) }
+  scope :upcoming, -> { where('date > ?', Time.now).order(:date) }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,4 @@ class User < ApplicationRecord
 
   has_many :event_attendances, foreign_key: :attendee_id, dependent: :destroy
   has_many :attended_events, through: :event_attendances, dependent: :destroy
-
-  def past_events
-    attended_events.where('date < ?', Time.now)
-  end
-
-  def upcoming_events
-    attended_events.where('date > ?', Time.now)
-  end
 end
